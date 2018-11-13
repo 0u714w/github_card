5780a5d25e84b2ddd716496c64aa5b6daa06dcd5
Doug Enas <douglassenas@gmail.com>
2018-11-13 09:52:11 -0500

    Updates

---
+++ precache-manifest.f2de7cc86f2b637a8b066037579942a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github_cards/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d47ff2de7b4ed61fca3e",
+    "url": "/github_cards/static/js/runtime~main.d47ff2de.js"
+  },
+  {
+    "revision": "0ed1ef26d53f5bf37a2b",
+    "url": "/github_cards/static/js/main.0ed1ef26.chunk.js"
+  },
+  {
+    "revision": "61b4854f117914a4fcec",
+    "url": "/github_cards/static/js/1.61b4854f.chunk.js"
+  },
+  {
+    "revision": "0ed1ef26d53f5bf37a2b",
+    "url": "/github_cards/static/css/main.4c35eb79.chunk.css"
+  },
+  {
+    "revision": "6e25eb02cd2d93aed6cd2bc368a1b092",
+    "url": "/github_cards/index.html"
+  }
+];
