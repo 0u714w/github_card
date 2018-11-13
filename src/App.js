1cc22266efaa1098296c288ecaabe63f0d70c7f9
Doug Enas <douglassenas@gmail.com>
2018-11-13 09:48:01 -0500

    first

--- src/App.js
+++ src/App.js
@@ -3,26 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { active: false, user: {} }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+
+  handleClick() {
+
+
+    fetch('https://api.github.com/users/dougenas')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ user: data })
+        console.log(this.state)
+      })
+
+    this.setState(prevState => ({
+      active: !prevState.active
+    }));
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+        <button onClick={this.handleClick}>Click Me</button>
+        {this.state.active ?
+          < div id='main' >
+            <img src={this.state.user.avatar_url} />
+            <div>{this.state.user.name}</div>
+            <div>Following: {this.state.user.following}</div>
+            <div>Url: {this.state.user.html_url}</div>
+          </div>
+          :
+          null}
       </div>
     );
   }
 }
+    
 
 export default App;
